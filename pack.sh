950cf167e8e29d87fe325305d04c71e96ce7fa50
sivakumarkatam <siva.katam@blazeclan.com>
2017-04-21 16:23:29 +0530

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu
-sudo cp /var/lib/jenkins/workspace/packer/* /home/ubuntu/packer/
+#sudo cp /var/lib/jenkins/workspace/packer/* /home/ubuntu/packer/
 cd /home/ubuntu/packer
 packer build coe.json
