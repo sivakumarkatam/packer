959a7b18849bcc578f7c6ce6a59dcef0b5d75f7e
sivakumarkatam <siva.katam@blazeclan.com>
2017-05-02 14:42:03 +0530

    Update apgscript.sh

--- apgscript.sh
+++ apgscript.sh
@@ -14,4 +14,4 @@ cd /home/ubuntu
 wget https://aws-codedeploy-ap-southeast-1.s3.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto
-sudo service codedeploy-agent start
+sudo systemctl start codedeploy-agent
