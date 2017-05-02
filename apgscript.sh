47d14cc5a57260a28e4f6de7c5db2d696406d142
sivakumarkatam <siva.katam@blazeclan.com>
2017-05-02 11:10:10 +0530

    Update apgscript.sh

--- apgscript.sh
+++ apgscript.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo su -
 sudo yum -y update
 sudo yum -y install wget
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie"  "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jre-8u131-linux-x64.rpm"
