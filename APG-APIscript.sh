89e1cd29af58f5add9a6df5e4ce748d2953f0295
sivakumarkatam <siva.katam@blazeclan.com>
2017-06-01 07:39:14 +0530

    Update APG-APIscript.sh

--- APG-APIscript.sh
+++ APG-APIscript.sh
@@ -9,11 +9,8 @@ wget https://aws-codedeploy-ap-southeast-1.s3.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto
 sudo service codedeploy-agent start
-sudo apt-get install python3 -y
+sudo apt-get install -y python3
 
-sudo apt-add-repository ppa:ansible/ansible -y
-sudo apt-get update
-sudo apt-get install ansible -y
 
 cd /home/ubuntu
 wget https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py
@@ -23,18 +20,9 @@ pwd
 echo '[general]
 state_file = /var/awslogs/state/agent-state  
  
-[/home/ubuntu/cloudwatch/logs/Debug.log]
-
-datetime_format = %Y/%m/%d %H:%M:%S
-file = /home/ubuntu/cloudwatch/logs/Debug.log
-buffer_duration = 5000
-log_stream_name = API-Debug
-initial_position = end_of_file
-log_group_name = /home/ubuntu/cloudwatch/logs/Debug.log 
-
 [/var/log/messages]
 datetime_format = %Y/%m/%d %H:%M:%S
-file = /var/log/messages
+file = /var/log/syslog
 buffer_duration = 5000
 log_stream_name = API-Syslogs
 initial_position = end_of_file
@@ -53,9 +41,9 @@ ExecStart=/var/awslogs/bin/awslogs-agent-launcher.sh --start --background --pidf
 [Install]
 WantedBy=multi-user.target' >> serv
 
-sudo python3 ./awslogs-agent-setup.py -c conf -r ap-southeast-1 -n
+sudo python3 ./awslogs-agent-setup.py -c /home/ubuntu/conf -r ap-southeast-1 -n
 cd /etc/systemd/system
 sudo cp /home/ubuntu/serv /etc/systemd/system/awslogs.service
-sudo systemctl daemon-reload
 sudo systemctl enable awslogs.service
 sudo systemctl start awslogs.service
+
