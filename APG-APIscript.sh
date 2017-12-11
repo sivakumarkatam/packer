1fabd5bb80560aef56d3764b06d26d5b41e39412
sivakumarkatam <siva.katam@blazeclan.com>
2017-12-11 17:51:25 +0530

    Update APG-APIscript.sh

--- APG-APIscript.sh
+++ APG-APIscript.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
 env='UAT'
 sudo su
-sudo yum -y update
-sudo yum -y install wget rpm epel-release awscli ruby python
-sudo mkdir -i /var/log/unicorn
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie"  "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jre-8u131-linux-x64.rpm"
-
-sudo yum -y localinstall jre-8u131-linux-x64.rpm
-
-sudo yum -y install ntp
+sudo yum update -y
+sudo yum install -y epel-release wget awscli ruby python nginx ntp zip unzip
+wget http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm
+sudo rpm -ivh nginx-release-centos-7-0.el7.ngx.noarch.rpm
+#sudo yum install -y nginx
+sudo rm -rf nginx-release-centos-7-0.el7.ngx.noarch.rpm
+sudo service nginx start
+sudo mkdir -u /var/log/unicorn
+sudo chkconfig nginx on
+#sudo yum install ntp -y
 sudo timedatectl set-timezone Asia/Singapore
 sudo systemctl enable ntpd
 sudo systemctl disable chronyd
 sudo systemctl start ntpd
-
-sudo rm -rf jre-8u131-linux-x64.rpm
-
-wget http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm
-sudo rpm -ivh nginx-release-centos-7-0.el7.ngx.noarch.rpm
-sudo yum install -y nginx
-sudo rm -rf nginx-release-centos-7-0.el7.ngx.noarch.rpm
-sudo service nginx start
-sudo chkconfig nginx on
-
 
 cd /tmp/
 wget https://aws-codedeploy-ap-southeast-1.s3.amazonaws.com/latest/install
@@ -45,24 +37,24 @@ file = /var/log/nginx/error.log
 buffer_duration = 5000
 log_stream_name = NginxErrorLogs
 initial_position = end_of_file
-log_group_name = $env-UNICORN-API-logs
+log_group_name = $env-UNICORN-UI-logs
 
 [/var/log/syslogs]
 datetime_format = %Y/%m/%d %H:%M:%S
 file = /var/log/messages
 buffer_duration = 5000
-log_stream_name = API-Syslogs
+log_stream_name = UI-Syslogs
 initial_position = end_of_file
-log_group_name = $env-UNICORN-API-logs
+log_group_name = $env-UNICORN-UI-logs
 
 [/var/log/unicorn]
 
 datetime_format = %Y/%m/%d %H:%M:%S
-file = /var/log/unicorn/api.log
+file = /var/log/unicorn/ui.log
 buffer_duration = 5000
-log_stream_name = API-Debuglogs
+log_stream_name = UI-Debuglogs
 initial_position = end_of_file
-log_group_name = $env-UNICORN-API-logs" >> /tmp/conf
+log_group_name = $env-UNICORN-UI-logs" >> /tmp/config
 
 #echo '[Unit]
 #Description=Service for CloudWatch Logs agent
@@ -77,7 +69,7 @@ log_group_name = $env-UNICORN-API-logs" >> /tmp/conf
 #[Install]
 #WantedBy=multi-user.target' >> /home/centos/serv
 
-sudo ./awslogs-agent-setup.py -c /tmp/conf -r ap-southeast-1 -n
+sudo ./awslogs-agent-setup.py -c /tmp/config -r ap-southeast-1 -n
 #cd /etc/systemd/system
 #sudo cp /home/centos/serv /etc/systemd/system/awslogs.service
 sudo systemctl daemon-reload
@@ -87,7 +79,7 @@ sudo systemctl enable awslogs.service
 sudo systemctl start awslogs.service
 cd /tmp/
 sudo yum install perl-Switch perl-DateTime perl-Sys-Syslog perl-LWP-Protocol-https perl-Digest-SHA -y
-sudo yum install zip unzip -y
+#sudo yum install zip unzip -y
 sudo curl http://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.1.zip -O
 sudo unzip CloudWatchMonitoringScripts-1.2.1.zip
 sudo rm -f CloudWatchMonitoringScripts-1.2.1.zip
