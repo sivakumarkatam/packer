6ab9c23561956ee0e69c9bb80ff87677c3c1797f
sivakumarkatam <siva.katam@blazeclan.com>
2018-04-17 17:48:55 +0530

    Update node.sh

--- node.sh
+++ node.sh
@@ -22,36 +22,47 @@ cat /etc/yum.repos.d/Gluster.repo
 sleep 10
 az=`curl http://169.254.169.254/latest/meta-data/placement/availability-zone/`
 sudo mkdir -p /var/www/magento2/pub/media
+touch /var/www/magento2/pub/media/new.html
 
 #echo "AZ is: $az"
 sleep 5
 
+
 if [ "$az" == "ap-southeast-1a" ]
  then
+ 
+grep -q -F 'gfs2.unicorn.cloud:/gv0 /var/www/magento2/pub/media/ glusterfs context=system_u:object_r:httpd_sys_rw_content_t:s0,defaults,_netdev,backupvolfile-server=gfs1.unicorn.cloud,fetch-attempts=10 0 2' /etc/fstab || echo 'gfs2.unicorn.cloud:/gv0 /var/www/magento2/pub/media/ glusterfs context=system_u:object_r:httpd_sys_rw_content_t:s0,defaults,_netdev,backupvolfile-server=gfs1.unicorn.cloud,fetch-attempts=10 0 2' >> /etc/fstab
 sudo mount -t glusterfs -o context=system_u:object_r:httpd_sys_rw_content_t:s0  gfs1.unicorn.cloud:/gv0 /var/www/magento2/pub/media/
 
-#sudo mount -t glusterfs gfs1.unicorn.cloud:/gv0 /var/www/magento2/pub/media/
-
 echo "AZ is: $az"
-#echo 'gfs1.unicorn.cloud:/gv0 /var/www/magento2/pub/media glusterfs defaults,_netdev backupvolfile-server=gfs2.unicron.cloud   0 0' >> /etc/fstab
-ABC=`sudo mount -t glusterfs -o context=system_u:object_r:httpd_sys_rw_content_t:s0  gfs1.unicorn.cloud:/gv0 /var/www/magento2/pub/media/`
-echo "$ABC"
+cat /etc/fstab
 elif [ "$az" == "ap-southeast-1b" ]
  then
-#ABC=`sudo mount -t glusterfs -o context=system_u:object_r:httpd_sys_rw_content_t:s0  gfs2.unicorn.cloud:/gv0 /var/www/magento2/pub/media/`
-#sudo mount -t glusterfs gfs1.unicorn.cloud:/gv0 /var/www/magento2/pub/media/
-echo "ABC"
+grep -q -F 'gfs2.unicorn.cloud:/gv0 /var/www/magento2/pub/media/ glusterfs context=system_u:object_r:httpd_sys_rw_content_t:s0,defaults,_netdev,backupvolfile-server=gfs1.unicorn.cloud,fetch-attempts=10 0 2' /etc/fstab || echo 'gfs2.unicorn.cloud:/gv0 /var/www/magento2/pub/media/ glusterfs context=system_u:object_r:httpd_sys_rw_content_t:s0,defaults,_netdev,backupvolfile-server=gfs1.unicorn.cloud,fetch-attempts=10 0 2' >> /etc/fstab
+sudo mount -t glusterfs -o context=system_u:object_r:httpd_sys_rw_content_t:s0  gfs2.unicorn.cloud:/gv0 /var/www/magento2/pub/media/
 echo "AZ is: $az"
-
+cat /etc/fstab
 elif [ "$az" == "ap-southeast-1c" ]
  then
-#sudo mount -t glusterfs -o context=system_u:object_r:httpd_sys_rw_content_t:s0  gfs3.unicorn.cloud:/gv0 /var/www/magento2/pub/media/
-#sudo mount -t glusterfs gfs1.unicorn.cloud:/gv0 /var/www/magento2/pub/media/
+
+sudo mount -t glusterfs -o context=system_u:object_r:httpd_sys_rw_content_t:s0  gfs3.unicorn.cloud:/gv0 /var/www/magento2/pub/media/
+grep -q -F 'gfs2.unicorn.cloud:/gv0 /var/www/magento2/pub/media/ glusterfs context=system_u:object_r:httpd_sys_rw_content_t:s0,defaults,_netdev,backupvolfile-server=gfs1.unicorn.cloud,fetch-attempts=10 0 2' /etc/fstab || echo 'gfs2.unicorn.cloud:/gv0 /var/www/magento2/pub/media/ glusterfs context=system_u:object_r:httpd_sys_rw_content_t:s0,defaults,_netdev,backupvolfile-server=gfs1.unicorn.cloud,fetch-attempts=10 0 2' >> /etc/fstab
 echo "AZ is: $az"
+cat /etc/fstab
 else
 
 exit 1
 
 fi
+
+sleep 10
+echo "mount -a command is running..."
+mount -a
+echo "sucessfully mounted..."
+df -h
+echo "Changing ownership of pub directory "
+chown -R nginx: /var/www/magento2/pub
+
+ls -Z /var/www/magento2/pub
 sudo chcon -R -t httpd_sys_rw_content_t /var/www/magento2/
 sudo df -h
